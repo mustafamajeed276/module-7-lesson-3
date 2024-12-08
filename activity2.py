ec28fe719c8402b20715a825508aac493387a6c6
mustafa276 <mmustafamajeed1@gmail.com>
2024-12-08 18:51:07 +0500

    homework

--- activity2.py
+++ activity2.py
@@ -1,13 +1,55 @@
+# Import necessary libraries
 from tkinter import *
-from tkinter import messagebox
+import datetime
+
+# Create Window
 root = Tk()
-root.geometry("200x200")
-def msg():
-    messagebox.showwarning("homework alert", "mom: if you do not do homework i will hack your computer")
-    messagebox.askyesno("homework alert", "mom: are you doing homework?")
-    messagebox.showinfo("homework alert", "mom: fine i will help: 8x9 = 72")
-    messagebox.askyesno("homework alert", "mom: do you want to go to school?")
-    messagebox.showwarning("homework alert", "mom: else i will kill you")
-button = Button(root, text = "skip homework", command = msg)
-button.place(x=40, y=80)
+root.title('Age Calculator App')
+root.geometry('400x400')
+
+# Create a frame to organize elements better
+frame = Frame(master=root, height=200, width=360, bg="#FAB072")
+
+# Add widgets
+# Add Label
+lbl1 = Label(frame, text="Name", bg="navyblue", fg='white', width=12)
+lbl2 = Label(frame, text="Year", bg="navyblue", fg='white', width=12)
+lbl3 = Label(frame, text="Month", bg="navyblue", fg='white', width=12)
+lbl4 = Label(frame, text="Date", bg="navyblue", fg='white', width=12)
+
+# Use Entry Widget to create a text box for user to enter details
+name_entry = Entry(frame)
+year_entry = Entry(frame)
+month_entry = Entry(frame)
+date_entry = Entry(frame)
+
+
+# Function to display message
+def calculate():
+  name = name_entry.get()
+  year = int(year_entry.get())
+  today = datetime.date.today()
+  age = today.year - year
+  greet = "Hey " + name
+  message = "\nYour age is: " + str(age)
+  textbox.insert(END, greet)
+  textbox.insert(END, message)
+
+
+# Textbox to display message
+textbox = Text(bg="gray", fg="white")
+
+# Add Button, when pressed, message will be displayed
+btn = Button(text="Calculate", command=calculate, bg="red")
+
+# Arrange all widgets
+frame.place(x=20, y=0)
+lbl1.place(x=20, y=20)
+name_entry.place(x=150, y=20)
+lbl2.place(x=20, y=50)
+year_entry.place(x=150, y=50)
+lbl3.place(x=20, y=80)
+month_entry.place(x=150, y=80)
+lbl4.place(x=20, y=110)
+date_entry.place(x=150, y=110)
 root.mainloop()
